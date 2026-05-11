394c08e81c63a1d876adf1d44f06a3cc1ea7f456
Uvarov1tte <ngqchia@gmail.com>
2026-05-11 22:44:53 +0700

    (ex9)

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,5 +1,5 @@
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://root:example@localhost:3456/the_database?authSource=admin' //just localhost so np
-const REDIS_URL = process.env.REDIS_URL || undefined
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 module.exports = {
   MONGO_URL,
